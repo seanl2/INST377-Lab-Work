66013bc184ee811ad72d7249f15b421944cffe87
Sean Leahy <sfleahy2000@gmail.com>
2022-11-22 18:18:55 -0500

    map

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -85,6 +85,10 @@ function filterList(array, filterInputValue) {
 function initMap() {
   console.log('initMap');
   const map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
 }
 
 async function mainEvent() {
