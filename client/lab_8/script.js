efb30dbb5c975b3ef85449b1a72fdaa510448871
Sean Leahy <sfleahy2000@gmail.com>
2022-11-22 18:47:41 -0500

    stuff

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -93,10 +93,16 @@ function initMap() {
 }
 
 function markerPlace(array, map) {
-  console.log('markerPlace', array);
-  const marker = L.marker([51.5, -0.09]).addTo(map);
+  // const marker = L.marker([51.5, -0.09]).addTo(map);
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
   array.forEach((item) => {
     const {coordinates} = item.geocoded_column_1;
+    console.log(item);
     L.marker([coordinates[1], coordinates[0]]).addTo(map);
   });
 }
